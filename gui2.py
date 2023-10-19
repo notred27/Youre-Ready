a45be012f30bc4282e6894fc4e1afbcbc0637079
notred27 <mikereidy95@gmail.com>
2023-10-19 15:39:14 -0400

    finsihed gui2

--- gui2.py
+++ gui2.py
@@ -22,60 +22,46 @@ def relative_to_assets(path: str) -> Path:
 
 
 window = Tk()
-
 window.geometry("696x400")
 window.configure(bg = "#F2F4F1")
 
 
 # Create something that merges the looks of the dropdown course menues with scrolable frames that act like comboboxes
 class CustomDropDown(Frame):
-    def __init__(self, parent,text,values, *args, **kw):
+    def __init__(self, parent,text,img, width,text_width, values,list_width=44, *args, **kw):
         Frame.__init__(self, parent, *args, **kw)
-        # parent.create_image(x,y, anchor = "nw", image= search_long_img)
-
-        self.canvas = Canvas(self, width = 286, height = 60, bg= "#7268A6", bd=0,highlightthickness=0)
-        self.canvas.create_image(2,2, anchor = "nw", image= search_long_img)
-        self.entry = Entry(self,bg="#B29EC6",fg= "#f2f4f1",font=("IstokWeb Bold", 20 * -1),bd=0, )
-        self.entry.place(in_=self.canvas, x=12, y = 11)
-
         self.text = text
-        self.entry.insert(0, self.text)
-
         self.values = values
 
-        # # self.canvas.create_text(12.0,11.0,anchor="nw",text = "Semester (REQUIRED):",fill="#F2F4F1",font=("IstokWeb Bold", 20 * -1))
-        # self.scroll = Scrollbar(self,orient="vertical", command=self.canvas.yview)
-        # self.scroll.grid(row=1, column=4, sticky='ns')
+        # Create canvas and entry box
+        self.canvas = Canvas(self, width = width, height = 60, bg= "#7268A6", bd=0,highlightthickness=0)
+        self.canvas.create_image(2,2, anchor = "nw", image= img)
+        self.entry = Entry(self,bg="#B29EC6",fg= "#f2f4f1",font=("IstokWeb Bold", 20 * -1),bd=0, width = text_width)
+        self.entry.place(in_=self.canvas, x=5, y = 27, anchor="w")
+        self.entry.insert(0, self.text)
 
-        # for i in range(3):
-        #     b = Label(self.scroll, text=str(i))
-        #     b.pack()
-        # self.scroll.pack()
-
-        # # self.scroll.place(in_= canvas, x=50,y=50)
+        
+        # Create frame and scroll bar for options
         self.scroll_frame = Frame(self)
         self.scroll_frame.pack_forget()
         scrollbar = Scrollbar(self.scroll_frame)
         scrollbar.pack( side = RIGHT, fill=Y )
 
-        self.list = Listbox(self.scroll_frame,width = 44, yscrollcommand = scrollbar.set, borderwidth=0, highlightthickness=0,selectmode=SINGLE)  #TODO: adaptive height when less than num elements?
-        # for line in range(100):
-        #     # self.list.insert(END, "This is line number " + str(line))
-        #     self.values.append( "This is line number " + str(line))
-        # # print(self.values)
+        self.list = Listbox(self.scroll_frame,width = list_width, yscrollcommand = scrollbar.set, borderwidth=0, highlightthickness=0,selectmode=SINGLE)  
         self.list.pack( side = LEFT,anchor="s", fill = BOTH )
-        scrollbar.config( command = self.list.yview )
-
+    
+        scrollbar.config(command = self.list.yview)
         self.canvas.pack(anchor="n",side=TOP)
-        # self.scroll_frame.pack(side=BOTTOM)
 
 
+        # Bind events
         self.entry.bind('<Button-1>', self.clear_text)
         self.entry.bind('<KeyRelease>', self.manage_input)
+        self.list.bind('<Button-1>', self.select_option)
 
-        self.list.bind('<Button-1>', self.select)
 
     def clear_text(self, event):
+        # Used to clear the starting text in th entry and toggle the drop down list
         if self.entry.get() == self.text:
             self.entry.delete(0,len(self.text)+ 1)
 
@@ -84,18 +70,18 @@ class CustomDropDown(Frame):
         self.scroll_frame.pack(side=BOTTOM)
 
 
-
     def manage_input(self, event):
+        # Used to change what is shown in hte drop down list
+        self.list.config(height= 11)
         value = self.entry.get()
         self.list.delete(0, self.list.size())
 
         if value == '':
             for i in range(len(self.values)):
                 self.list.insert(self.list.size(), self.values[i])
-            self.list.config(height= 11)
+            
 
         else:   #TODO try to condense following lines
-            
             data = []
             for item in self.values:
                 if value.lower() in item.lower():
@@ -104,147 +90,91 @@ class CustomDropDown(Frame):
             for i in range(len(data)):
                 self.list.insert(self.list.size(), data[i])
             
-            if len(data) < 11:
-                self.list.config(height= len(data))
-            else:
-                self.list.config(height= 11)
+
+        if len(data) < 11:
+            self.list.config(height= len(data))
+            
 
 
-    def toggle_drop(self, event):
-        print("toggle drop clicked")
-        self.scroll_frame.forget()
-
-    def select(self, event):
-        try:
-            self.entry.delete(0,len(self.entry.get()))
-            self.entry.insert(0, self.list.selection_get())
-            self.list.selection_clear(0, len(self.values))
-            self.scroll_frame.forget()
+    def select_option(self, event):
+        # Used to select a value from the drop down list
+        try:    #FIXME edge case when first selescting a value
+            if self.entry.get() != self.list.selection_get():
+                self.entry.delete(0,len(self.entry.get()))
+                self.entry.insert(0, self.list.selection_get())
+                self.list.selection_clear(0, len(self.values))
+                self.scroll_frame.forget()
         except:
             pass
        
 
 
-        # self.canvas.config(height=140 )
-
-        
-
-
-class CustomCombobox(ttk.Combobox):
-    def __init__(self, parent,values = None, startingText = '',fg = 'black',  *args, **kw):
-        ttk.Combobox.__init__(self, parent, *args, **kw)
-        # Label.__init__(self, parent, image=search_long_img)
-
-        
-        
-        self.values = values
-        self['values'] = values
-
-        self.startingText = startingText
-        self.set(startingText)
-        self.configure(foreground="#777777")
-
-        self.fg = fg
-
-        self.bind('<Button-1>', self.manage_initial_text)
-        self.bind('<KeyRelease>', self.manage_combobox)
-
-    def manage_initial_text(self, event):
-
-        if self.get() == self.startingText:
-            self.set('')
-            self.configure(foreground=self.fg)
-
-
-    def manage_combobox(self, event):
-        value = event.widget.get()
-
-        if value == '':
-            self['values'] = self.values
-
-        else:
-            data = []
-            for item in self.values:
-                if value.lower() in item.lower():
-                    data.append(item)
-            data.sort()
-            self['values'] = data
-
-    def reset(self):
-        self.set(self.startingText)
-        self.configure(foreground="#777777")
-        self['values'] = self.values
-
-
-
-
-canvas = Canvas(
-    window,
-    bg = "#F2F4F1",
-    height = 400,
-    width = 696,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-
-
+#Photo images for search widget
 bg_img = PhotoImage(file=relative_to_assets("search_bg.png"))
 search_btn_img = PhotoImage(file=relative_to_assets("search_btn.png"))
 search_long_img = PhotoImage(file=relative_to_assets("search_long.png"))
 search_short_img = PhotoImage(file=relative_to_assets("search_small.png"))
 
 
-
-
+canvas = Canvas(window,bg = "#F2F4F1",height = 400,width = 696,bd = 0,highlightthickness = 0,relief = "ridge")
 canvas.create_image(102,59, anchor = "nw", image= bg_img)
 
 
-
+# Search button for submitting form
 search_btn = Button(image=search_btn_img,borderwidth=0,highlightthickness=0,command=lambda: print("search btn clicked"),relief="flat")
-search_btn.place(x=447.0,y=234.0,width=103.0,height=60.0)
+search_btn.place(in_=canvas, x=447.0,y=234.0,width=103.0,height=60.0)
 
 # 1st long bar
-canvas.create_image(123,85, anchor = "nw", image= search_long_img)
+# canvas.create_image(123,85, anchor = "nw", image= search_long_img)
+
+semester_box = CustomDropDown(window,text = "Semester (REQUIRED):", values = load_dropbox, width = 286, img = search_long_img, text_width = 25)
+semester_box.place(in_=canvas, x=123,y=85)
+
 
 # 2nd long bar
-canvas.create_image(123,161, anchor = "nw", image= search_long_img)
+# canvas.create_image(123,161, anchor = "nw", image= search_long_img)
+
+subject_box = CustomDropDown(window,text = "Subject:", values = load_dropbox, width = 286, img = search_long_img, text_width = 25)
+subject_box.place(in_=canvas, x=123,y=161)
 
 # 3rd long bar
-canvas.create_image(123,237, anchor = "nw", image= search_long_img)
+# canvas.create_image(123,237, anchor = "nw", image= search_long_img)
+
+course_box = CustomDropDown(window,text = "Course Type:", values = load_dropbox, width = 286, img = search_long_img, text_width = 25)
+course_box.place(in_=canvas, x=123,y=237)
+
+
+
 
 # 1st short bar
-canvas.create_image(429,85, anchor = "nw", image= search_short_img)
+# canvas.create_image(429,85, anchor = "nw", image= search_short_img)
+id_box = CustomDropDown(window,text = "Course ID:", values = load_dropbox, width = 146, img = search_short_img, text_width = 12, list_width=21)
+id_box.place(in_=canvas, x=429,y=85)
 
 # 2nd short bar
 canvas.create_image(429,161, anchor = "nw", image= search_short_img)
+keywords_box = CustomDropDown(window,text = "Keywords:", values = load_dropbox, width = 146, img = search_short_img, text_width = 12, list_width=21)
+keywords_box.place(in_=canvas, x=429,y=161)
 
+# Checkbutton for hiding unavailable classes
 check_btn = Checkbutton(window,bd=0,highlightthickness=0, background="#7268A6", activebackground="#7268A6")
 check_btn.place(in_=canvas, x = 361,y =310)
-
-box = CustomCombobox(window, values=load_dropbox)
-box.place(in_=canvas, x=123,y=85)
-
-box.pack()
-
-
-cust = CustomDropDown(window,text = "Semester (REQUIRED):", values = load_dropbox)
-
-cust.place(in_=canvas, x=123,y=85)
-
-# cust.pack()
-
-canvas.create_text(135.0,249.0,anchor="nw",text="Course Type:",fill="#F2F4F1",font=("IstokWeb Bold", 20 * -1))
-
-canvas.create_text(439.0,173.0,anchor="nw",text="Keywords:",fill="#F2F4F1",font=("IstokWeb Bold", 20 * -1))
-
-canvas.create_text(439.0,96.0,anchor="nw",text="Course ID:",fill="#F2F4F1",font=("IstokWeb Bold", 20 * -1))
-
-canvas.create_text(135.0,173.0,anchor="nw",text="Subject:",fill="#F2F4F1",font=("IstokWeb Bold", 20 * -1))
-
-canvas.create_text(135.0,96.0,anchor="nw",text="Semester (REQUIRED):",fill="#F2F4F1",font=("IstokWeb Bold", 20 * -1))
-
 canvas.create_text(386.0,311.0,anchor="nw",text="Hide unavailable classes",fill="#FFFFFF",font=("IstokWeb Bold", 16 * -1))
+
+
+
+
+#Keep lift order so widgets don't obscure each other
+semester_box.lift(aboveThis=subject_box)
+semester_box.lift(aboveThis=course_box)
+subject_box.lift(aboveThis=course_box)
+semester_box.lift(aboveThis=check_btn)
+subject_box.lift(aboveThis=check_btn)
+course_box.lift(aboveThis=check_btn)
+id_box.lift(aboveThis=keywords_box)
+
+
+
 
 
 canvas.pack()
